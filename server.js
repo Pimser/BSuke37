9999fca3adaf36557dab5d6adcec3d555d4dd5a1
William Eugen <williamhellebustkromvoll@gmail.com>
2024-09-09 12:05:34 +0200

    first

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(process.env.PORT);
